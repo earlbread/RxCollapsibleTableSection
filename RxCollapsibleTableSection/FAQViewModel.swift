3468bd0fdd054681497d3ed0ca9a5759255a9901
Seunghun Lee <waydi1@gmail.com>
2017-07-24 23:55:05 +0900

    Add FAQ list

--- RxCollapsibleTableSection/FAQViewModel.swift
+++ RxCollapsibleTableSection/FAQViewModel.swift
@@ -8,6 +8,15 @@
 
 import Foundation
 
+var faqs = [
+  FAQItem(question: "What is Lorem Ipsum?",
+          answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry"),
+  FAQItem(question: "Where does it come from?",
+          answer: "It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."),
+  FAQItem(question: "Why do we use it?",
+          answer: "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.")
+]
+
 class FAQItem {
   var rowCount: Int {
     return 1
