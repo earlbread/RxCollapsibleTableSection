b5d034c0181e4a50343f297c41aae645cd879276
Seunghun Lee <waydi1@gmail.com>
2017-07-25 09:16:34 +0900

    Move faqs to ViewModel items

--- RxCollapsibleTableSection/FAQViewModel.swift
+++ RxCollapsibleTableSection/FAQViewModel.swift
@@ -8,20 +8,10 @@
 
 import Foundation
 
-var faqs = [
-  FAQItem(question: "What is Lorem Ipsum?",
-          answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry"),
-  FAQItem(question: "Where does it come from?",
-          answer: "It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."),
-  FAQItem(question: "Why do we use it?",
-          answer: "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.")
-]
-
 class FAQItem {
   var rowCount: Int {
     return 1
   }
-  var isCollapsed = true
 
   var question: String
   var answer: String
@@ -33,8 +23,15 @@ class FAQItem {
 }
 
 class FAQViewModel {
-  var items: [FAQItem]
-
+  var items = [
+    FAQItem(question: "What is Lorem Ipsum?",
+            answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry"),
+    FAQItem(question: "Where does it come from?",
+            answer: "It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."),
+    FAQItem(question: "Why do we use it?",
+            answer: "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.")
+  ] 
+  
   init(items: [FAQItem]) {
     self.items = items
   }
