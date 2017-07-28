5c525ebdee7059d63a18efe421f105c9bff61f4d
Seunghun Lee <waydi1@gmail.com>
2017-07-28 23:06:03 +0900

    Make sections and connect with table view

--- RxCollapsibleTableSection/RxCollapsibleTableSectionViewController.swift
+++ RxCollapsibleTableSection/RxCollapsibleTableSectionViewController.swift
@@ -33,9 +33,32 @@ struct Section {
 
 class RxCollapsibleTableSectionViewController: UIViewController {
 
+  let disposeBag = DisposeBag()
+  let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, String>>()
+
   @IBOutlet weak var tableView: UITableView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    let sections = [
+      SectionModel(model: "Section1", items: ["Item 1-1", "Item 1-2", "Item 1-3"]),
+      SectionModel(model: "Section2", items: ["Item 2-1", "Item 2-2", "Item 2-3"]),
+      SectionModel(model: "Section3", items: ["Item 3-1", "Item 3-2", "Item 3-3"])
+    ]
+
+    dataSource.configureCell = { (dataSource, tableView, indexPath, sectionItem) in
+      guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ItemCell.self), for: indexPath) as? ItemCell else {
+        fatalError("Could not dequeue cell with ItemCell")
+      }
+
+      cell.titleLabel.text = sectionItem
+
+      return cell
     }
+
+    Observable.just(sections)
+      .bind(to: tableView.rx.items(dataSource: dataSource))
+      .disposed(by: disposeBag)
+  }
 
 }
